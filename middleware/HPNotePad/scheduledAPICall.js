6d85379171091f532a0fe17cfb6aa71956b40882
Patrick O'Brien <obrienp2321@gmail.com>
2023-04-28 07:36:13 +0200

    cron job to 7.30 am every day

--- middleware/HPNotePad/scheduledAPICall.js
+++ middleware/HPNotePad/scheduledAPICall.js
@@ -3,10 +3,29 @@ const schedule = require("node-schedule");
 const nodeCron = require("node-cron");
 const connectDB = require("../../config/connectDB");
 
+// function scheduledAPICall() {
+//   nodeCron.schedule("0 6 * * * ", function logUpdateToServer() {
+//     try {
+//       axios.post("https://activeserver.onrender.com/notepad/data");
+//     } catch (error) {
+//       if (error.response) {
+//         console.log(error.response.data);
+//         console.log(error.response.status);
+//         console.log(error.response.headers);
+//       } else if (error.request) {
+//         console.log(error.request);
+//       } else {
+//         console.log("Error", error.message);
+//       }
+//       console.log("error within scheduledAPICall");
+//     }
+//   });
+// }
+
 function scheduledAPICall() {
-  nodeCron.schedule("0 6 * * * ", function logUpdateToServer() {
+  nodeCron.schedule("30 7 * * *", function logUpdateToServer() {
     try {
-      axios.post("https://activeserver.onrender.com/notepad/data");
+      axios.post("http://localhost:5000/notepad/data");
     } catch (error) {
       if (error.response) {
         console.log(error.response.data);
