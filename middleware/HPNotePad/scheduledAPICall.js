fa112bf37f0c9fa0d521c1451923822c6aa0b471
Patrick O'Brien <obrienp2321@gmail.com>
2023-04-28 07:37:14 +0200

    update axios cronjob to activeServer

--- middleware/HPNotePad/scheduledAPICall.js
+++ middleware/HPNotePad/scheduledAPICall.js
@@ -3,29 +3,10 @@ const schedule = require("node-schedule");
 const nodeCron = require("node-cron");
 const connectDB = require("../../config/connectDB");
 
-// function scheduledAPICall() {
-//   nodeCron.schedule("0 6 * * * ", function logUpdateToServer() {
-//     try {
-//       axios.post("https://activeserver.onrender.com/notepad/data");
-//     } catch (error) {
-//       if (error.response) {
-//         console.log(error.response.data);
-//         console.log(error.response.status);
-//         console.log(error.response.headers);
-//       } else if (error.request) {
-//         console.log(error.request);
-//       } else {
-//         console.log("Error", error.message);
-//       }
-//       console.log("error within scheduledAPICall");
-//     }
-//   });
-// }
-
 function scheduledAPICall() {
-  nodeCron.schedule("30 7 * * *", function logUpdateToServer() {
+  nodeCron.schedule("30 7 * * * ", function logUpdateToServer() {
     try {
-      axios.post("http://localhost:5000/notepad/data");
+      axios.post("https://activeserver.onrender.com/notepad/data");
     } catch (error) {
       if (error.response) {
         console.log(error.response.data);
@@ -40,6 +21,26 @@ function scheduledAPICall() {
     }
   });
 }
+
+//testing
+// function scheduledAPICall() {
+//   nodeCron.schedule("30 7 * * *", function logUpdateToServer() {
+//     try {
+//       axios.post("http://localhost:5000/notepad/data");
+//     } catch (error) {
+//       if (error.response) {
+//         console.log(error.response.data);
+//         console.log(error.response.status);
+//         console.log(error.response.headers);
+//       } else if (error.request) {
+//         console.log(error.request);
+//       } else {
+//         console.log("Error", error.message);
+//       }
+//       console.log("error within scheduledAPICall");
+//     }
+//   });
+// }
 
 
 module.exports = scheduledAPICall;
